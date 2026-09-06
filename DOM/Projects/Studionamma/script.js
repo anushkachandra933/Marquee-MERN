1796428251d4694b954e681a69e8694b453bdb47
anushkachandra933 <anushkachandra933@gmail.com>
2026-09-06 17:37:10 +0530

    created services section

--- DOM/Projects/Studionamma/script.js
+++ DOM/Projects/Studionamma/script.js
@@ -2,6 +2,7 @@ let cursorBox = document.getElementById("animated-cursor-box");
 let topSection = document.getElementById("top-section");
 let cursorFollower = document.getElementById("follower");
 let interestOptionItems = document.querySelectorAll(".interest-option-item");
+let titleText = document.querySelector("#title");
 let modeChangeText = document.getElementById("mode");
 let menuText = document.getElementById("menu");
 let overlayMenus = document.getElementById("overlay");
@@ -12,11 +13,10 @@ let contactText = document.getElementById("contact");
 let contactFormOverlay = document.getElementById("contact-form-overlay");
 let navbar = document.getElementById("nav-ribbon");
 
-document.addEventListener("mousemove", (e) => {
-  
+document.addEventListener("mousemove", (e) => {  
   cursorFollower.style.left = `${e.clientX}px `;
   cursorFollower.style.top = `${e.clientY}px`;
-  cursorFollower.style.transform = "translate(-30px,-30px)";
+  cursorFollower.style.transform = "translate(-20px,-20px)";
 });
 
 // Navbar
@@ -26,22 +26,8 @@ modeChangeText.addEventListener("click", (e) => {
     ? (e.target.textContent = "LIGHT MODE")
     : (e.target.textContent = "DARK MODE");
 
-  // To change the BG of html page
-  this.document.body.classList.toggle("dark");
-
-  overlayHeading.forEach((menu) => {
-    if (isDarkMode) {
-      document.getElementById("nav-ribbon").style.color = "white";
-      topSection.style.backgroundColor = "black";
-      overlayMenus.style.backgroundColor = "black";
-      overlayMenus.style.color = "white";
-    } else {
-      document.getElementById("nav-ribbon").style.color = "black";
-      topSection.style.backgroundColor = "#e4e4e4";
-      overlayMenus.style.backgroundColor = "white";
-      overlayMenus.style.color = "black";
-    }
-  });
+  document.body.classList.toggle("dark", isDarkMode); //if isDarkMode is true,then add dark class
+  
 });
 
 // Top Section
@@ -74,35 +60,38 @@ menuText.addEventListener("mouseleave", (e) => {
   }
 });
 
-/*FIXME:  OVERLAY: STEP 1*/
+/*  OVERLAY: STEP 1*/
 menuText.addEventListener("click", (e) => {
   overlayIsOpen = !overlayIsOpen;
 
   if (overlayIsOpen) {
     e.target.textContent = "CLOSE";
-    overlayMenus.style.opacity = 1;
     overlayMenus.style.top = "0%";
+    overlayMenus.style.transform = "rotate(0deg)";
+    document.querySelectorAll(".overlay-item").forEach((item)=>{
+      item.style.transform = "scaleY(1.4)";
+    })
   } else {
     e.target.textContent = "MENU";
-    overlayMenus.style.opacity = 1;
-    overlayMenus.style.top = "-110%";
+    overlayMenus.style.top = "-135%";
+    overlayMenus.style.transform = "rotate(20deg)";
+    document.querySelectorAll(".overlay-item").forEach((item)=>{
+      item.style.transform = "scaleY(0)";
+    })
   }
   
 });
 
 // Move Left all the Heading of Overlays
-// FIXME: Overlay Items
 let overlayItems = document
   .querySelectorAll(".overlay-item")
   .forEach((item) => {
-    console.log(item);
     item.addEventListener("mouseenter", (e) => {
       item.firstElementChild.style.width = "160px";
     });
 
     item.addEventListener("mouseleave", (e) => {
       item.firstElementChild.style.width = "0px";
-      item.lastElementChild.style.transform = "translateX(0px)";
     });
   });
 
@@ -116,15 +105,64 @@ overlayMenus.addEventListener("mouseleave", (e) => {
   cursorBox.style.display = "block";
 });
 
+
+// Overlays Contact Text
+
+let contactOverlayIsOpen = false;
+
+contactText.addEventListener("mouseenter", (e) => {
+  if(contactOverlayIsOpen){
+    e.target.textContent = "CLOSE";
+  }else{
+    e.target.textContent = "CONTACT US";
+  }
+});
+
+contactText.addEventListener("mouseleave", (e) => {
+  if(contactOverlayIsOpen){
+    e.target.textContent = "CLOSE";
+  }else{
+    e.target.textContent = "LET'S TALK"
+  }
+});
+
+contactText.addEventListener("click", (e) => {
+  contactOverlayIsOpen = !contactOverlayIsOpen;
+  if (contactOverlayIsOpen) {
+    modeChangeText.style.display = "none";
+    titleText.style.display = "none";
+    menuText.style.display = "none";
+    contactText.style.transform = "translateX(450px)";
+    e.target.textContent = "CLOSE";
+    contactFormOverlay.style.top = "0%";
+    document.querySelector("#contact-form-overlay-heading img").style.width = "180px";
+  } else {
+    modeChangeText.style.display = "block";
+    titleText.style.display = "block";
+    menuText.style.display= "block";
+    contactText.style.transform = "none";
+    e.target.textContent = "LET'S TALK";
+    contactFormOverlay.style.top = "-100%";
+    document.querySelector("#contact-form-overlay-heading img").style.width = "0px";
+  }
+});
+
+interestOptionItems.forEach((option) => {
+  option.addEventListener("click", (e) => {
+    interestOptionItems.forEach((el) => el.classList.remove("active"));
+    e.target.classList.add("active");
+  });
+});
+
 // PopOut Area
-//TODO: Explanation PopOut Area
 
 const ImageTriggersBox = document.querySelectorAll('.highlight');
 
 ImageTriggersBox.forEach((element) => {
   let interval;
-  let index = 0;
-  //NOTE: When Mouseenter
+  let index;
+
+  //When Mouseenter
   element.addEventListener('mouseenter', () => {
     const images = element.querySelectorAll('.image-highlight');
 
@@ -150,7 +188,6 @@ ImageTriggersBox.forEach((element) => {
 
   // NOTE: when mouseleave
   element.addEventListener('mouseleave', () => {
-    // console.log(interval);
 
     // Stop the loop
     clearInterval(interval);
@@ -166,84 +203,39 @@ ImageTriggersBox.forEach((element) => {
   });
 });
 
-// Overlays Contact Text
 
-contactText.addEventListener("mouseenter", (e) => {
-  e.target.textContent = "CONTACT US";
-});
-contactText.addEventListener("mouseleave", (e) => {
-  e.target.textContent = "LET'S TALK";
-});
-
-let contactOverlayIsOpen = false;
-contactText.addEventListener("click", (e) => {
-  contactOverlayIsOpen = !contactOverlayIsOpen;
-  if (contactOverlayIsOpen) {
-    e.target.textContent = "CONTACT US";
-    contactFormOverlay.style.top = "0%";
-    document.querySelector("#gif-heading img").style.width = "200px";
-  } else {
-    e.target.textContent = "LET'S TALK";
-    contactFormOverlay.style.top = "-100%";
-    document.querySelector("#gif-heading img").style.width = "0px";
-  }
-});
-
-interestOptionItems.forEach((option) => {
-  option.addEventListener("click", (e) => {
-    interestOptionItems.forEach((el) => el.classList.remove("active"));
-    e.target.classList.add("active");
-  });
-});
-
-// TODO: Playgroud
+// Playgroud
 
 let playgroundHeading = document.getElementById('playground-heading');
-// let playgroundItem = ;
 
-playgroundHeading.addEventListener('mousemove', (e) => {
-  const { offsetX, offsetY } = e;
-
-  // FIXME: Move Item
-  document.getElementById('playground-item').style.top = `${offsetY}px`;
-  document.getElementById('playground-item').style.left = `${offsetX}px`;
+playgroundHeading.addEventListener('mouseenter', (e) => {
+  //  follower changed to portfolio 
+  cursorFollower.innerText = "PORTFOLIO";
+  cursorFollower.style.width = "120px";
+  cursorFollower.style.height = "50px";
+  cursorFollower.style.color = "white";
+  cursorFollower.style.borderRadius = "10px";
+  cursorFollower.style.fontSize = "1.9rem";
+  cursorFollower.style.fontFamily = "'Bebas Neue', sans-serif";
+  cursorFollower.style.padding = "9px";
+  cursorFollower.style.fontWeight = "800";
 });
 
-playgroundHeading.addEventListener('mouseenter',()=>{
-  document.getElementById('playground-item').style.display = 'block';
-})
-
 playgroundHeading.addEventListener('mouseleave',()=>{
-  document.getElementById('playground-item').style.display = 'none';
+  cursorFollower.innerHTML = "";
+  cursorFollower.style.width = "20px";
+  cursorFollower.style.height = "20px";
+  cursorFollower.style.borderRadius = "50%";
 })
 
 //grid-video-items
+
 document.querySelectorAll('.grid-vedio-items').forEach((item)=>{
   const follower = item.querySelector(".video-item-follower");
   item.addEventListener('mouseenter',(e)=>{
     e.target.firstElementChild.style.filter = "blur(3px) "
     e.target.style.transform = "scale(0.95)"
     e.target.lastElementChild.style.width = "80%";
-
-  })
-
-  item.addEventListener('mouseleave',(e)=>{
-    follower.style.display = "none";
-    e.target.firstElementChild.style.filter = "none";
-    e.target.style.transform = "scale(1)";
-    e.target.lastElementChild.style.width = "0";
-  })
-
-  //follower
-  item.addEventListener('mousemove',(e)=>{
-    follower.style.display = "block";
-    const rect = item.getBoundingClientRect();
-
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
-
-    follower.style.top = `${y}px`;
-    follower.style.left = `${x}px`;
   })
 
   setTimeout(() => {
@@ -251,3 +243,102 @@ document.querySelectorAll('.grid-vedio-items').forEach((item)=>{
     item.style.transform = "rotate(0deg)"
   }, 400);
 })
+
+//follower changed to matera
+let gridImageOne = document.querySelector('#grid-image-1');
+gridImageOne.addEventListener('mouseenter', (e) => {
+  
+  cursorFollower.innerText = "MATERA";
+  cursorFollower.style.width = "90px";
+  cursorFollower.style.height = "50px";
+  cursorFollower.style.color = "white";
+  cursorFollower.style.borderRadius = "10px";
+  cursorFollower.style.fontSize = "1.9rem";
+  cursorFollower.style.fontFamily = "'Bebas Neue', sans-serif";
+  cursorFollower.style.padding = "9px";
+  cursorFollower.style.fontWeight = "800";
+});
+
+gridImageOne.addEventListener('mouseleave',(e)=>{
+  e.target.firstElementChild.style.filter = "none";
+  e.target.style.transform = "scale(1)";
+  e.target.lastElementChild.style.width = "0";
+  cursorFollower.innerHTML = "";
+  cursorFollower.style.width = "20px";
+  cursorFollower.style.height = "20px";
+  cursorFollower.style.borderRadius = "50%";
+})
+
+//follower changed to matera
+let gridImageTwo = document.querySelector('#grid-image-2');
+gridImageTwo.addEventListener('mouseenter', (e) => {
+  
+  cursorFollower.innerText = "CHANCE";
+  cursorFollower.style.width = "90px";
+  cursorFollower.style.height = "50px";
+  cursorFollower.style.color = "white";
+  cursorFollower.style.borderRadius = "10px";
+  cursorFollower.style.fontSize = "1.9rem";
+  cursorFollower.style.fontFamily = "'Bebas Neue', sans-serif";
+  cursorFollower.style.padding = "9px";
+  cursorFollower.style.fontWeight = "800";
+});
+
+gridImageTwo.addEventListener('mouseleave',(e)=>{
+  e.target.firstElementChild.style.filter = "none";
+  e.target.style.transform = "scale(1)";
+  e.target.lastElementChild.style.width = "0";
+  cursorFollower.innerHTML = "";
+  cursorFollower.style.width = "20px";
+  cursorFollower.style.height = "20px";
+  cursorFollower.style.borderRadius = "50%";
+})
+
+let gridImageThree = document.querySelector('#grid-image-3');
+gridImageThree.addEventListener('mouseenter', (e) => {
+  //  Move follower
+  cursorFollower.innerText = "SILVR";
+  cursorFollower.style.width = "70px";
+  cursorFollower.style.height = "50px";
+  cursorFollower.style.color = "white";
+  cursorFollower.style.borderRadius = "10px";
+  cursorFollower.style.fontSize = "1.9rem";
+  cursorFollower.style.fontFamily = "'Bebas Neue', sans-serif";
+  cursorFollower.style.padding = "9px";
+  cursorFollower.style.fontWeight = "800";
+});
+
+gridImageThree.addEventListener('mouseleave',(e)=>{
+  e.target.firstElementChild.style.filter = "none";
+  e.target.style.transform = "scale(1)";
+  e.target.lastElementChild.style.width = "0";
+  cursorFollower.innerHTML = "";
+  cursorFollower.style.width = "20px";
+  cursorFollower.style.height = "20px";
+  cursorFollower.style.borderRadius = "50%";
+})
+
+let gridImageFour = document.querySelector('#grid-image-4');
+gridImageFour.addEventListener('mouseenter', (e) => {
+  //  Move follower
+  cursorFollower.innerText = "INTRAMUROS";
+  cursorFollower.style.width = "140px";
+  cursorFollower.style.height = "50px";
+  cursorFollower.style.color = "white";
+  cursorFollower.style.borderRadius = "10px";
+  cursorFollower.style.fontSize = "1.9rem";
+  cursorFollower.style.fontFamily = "'Bebas Neue', sans-serif";
+  cursorFollower.style.padding = "9px";
+  cursorFollower.style.fontWeight = "800";
+});
+
+gridImageFour.addEventListener('mouseleave',(e)=>{
+  e.target.firstElementChild.style.filter = "none";
+  e.target.style.transform = "scale(1)";
+  e.target.lastElementChild.style.width = "0";
+  cursorFollower.innerHTML = "";
+  cursorFollower.style.width = "20px";
+  cursorFollower.style.height = "20px";
+  cursorFollower.style.borderRadius = "50%";
+})
+
